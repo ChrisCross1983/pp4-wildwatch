66bddb994ef3018a79736def3d32a0a39c635fa3
Chris <christian.borza@students.codeinstitute.net>
2024-12-11 17:57:22 +0100

    Change the parameters for connecting to heroku db

--- wild_watch/settings.py
+++ wild_watch/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'wild-watch.herokuapp.com', 'wild-watch-4ac96b54e024.herokuapp.com', '*.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'wild-watch.herokuapp.com', 'wild-watch-4ac96b54e024.herokuapp.com', '.herokuapp.com']
 
 # Application definition
 
