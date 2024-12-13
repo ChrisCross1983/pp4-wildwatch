1471717e8cbff13c364a09bb5e0a092d95c0bf33
Chris <christian.borza@students.codeinstitute.net>
2024-12-13 18:54:31 +0100

    Add email backend to send real mails for reset password

--- wild_watch/settings.py
+++ wild_watch/settings.py
@@ -81,7 +81,15 @@ WSGI_APPLICATION = 'wild_watch.wsgi.application'
 LOGIN_URL = '/users/login/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#Password Reset Config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'WildWatch <cborza83@gmail.com>'
 
 
 # Database
