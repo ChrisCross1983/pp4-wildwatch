a1d1b9f67fb6a6785218d07c403abf45c4f4fa51
Chris <christian.borza@students.codeinstitute.net>
2024-12-27 06:40:57 +0100

    Set debug to true because of troubleshooting

--- wild_watch/settings.py
+++ wild_watch/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
