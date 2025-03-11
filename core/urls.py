1ebbc661fe0a591a828f2c12e4cde3fb54c10e03
Chris <christian.borza@students.codeinstitute.net>
2025-03-11 15:28:50 +0100

    Fix some more lighthouse perfomance errors like robot.txt and wrong order of headers in my_reports.html

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from users import views as user_views
+from .views import robots_txt_view
 
 urlpatterns = [
     path('', user_views.home, name='home'),
+    path("robots.txt", robots_txt_view, name="robots_txt"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to WildWatch")
+
+def robots_txt_view(request):
+    return HttpResponse("User-agent: *\nDisallow:", content_type="text/plain")
